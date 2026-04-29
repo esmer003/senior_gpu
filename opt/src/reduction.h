a68999f1807f737cba943ce23f8bdfec3fed53b8
esmer003 <126800729+esmer003@users.noreply.github.com>
2026-04-29 22:00:05 +0000

    fixed reduction file

--- opt/src/reduction.h
+++ opt/src/reduction.h
@@ -1,9 +1,6 @@
-void gaf_kernel
-(
-    float *g_a,
-    float *g_b,
-    float *final_dot,
-    float *final_norm_ga,
-    float *final_norm_gb,
-    int n
-);
+#ifndef REDUCTION_H
+#define REDUCTION_H
+
+void reduce_sum(float *input, float *partial, int n);
+
+#endif
