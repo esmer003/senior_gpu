318fc5bb3d7e4e0bce91956aa26de35a1d1367f2
esmer003 <126800729+esmer003@users.noreply.github.com>
2026-04-29 22:28:22 +0000

    revised reduction file

--- opt/src/reduction.h
+++ opt/src/reduction.h
@@ -1,6 +1,11 @@
 #ifndef REDUCTION_H
 #define REDUCTION_H
 
-void reduce_sum(float *input, float *partial, int n);
+#include <cuda_runtime.h>
+
+#ifdef __CUDACC__
+// Only the CUDA compiler will see this
+__global__ void reduce_sum(float *input, float *partial, int n);
+#endif
 
 #endif
