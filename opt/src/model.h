db76ee9d672df3d9a33bf1a897c52be3e24d16fb
esmer003 <126800729+esmer003@users.noreply.github.com>
2026-04-29 22:51:31 +0000

    revised model and reduction file

--- opt/src/model.h
+++ opt/src/model.h
@@ -1,6 +1,11 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#define N 1024
+#define BLOCK_SIZE 256
+#define EPOCHS 1000
+#define LEARNING_RATE 0.001f
+
 // If this is a CUDA kernel, it MUST have __global__
 __global__ void gradient_descent(
     const float *X,
--- opt/src/reduction.h
+++ opt/src/reduction.h
@@ -1,5 +1,8 @@
 #ifndef REDUCTION_H
 #define REDUCTION_H
 
+#include <cuda_runtime.h>
+
+__global__ void reduce_sum(float *input, float *partial, int n);
 
 #endif
