e24206dc1bd15c3eaae7a08bfa38d2f7955f1522
esmer003 <126800729+esmer003@users.noreply.github.com>
2026-05-01 02:44:44 +0000

    zero

--- opt/src/model.h
+++ opt/src/model.h
@@ -4,7 +4,7 @@
 #define N 1024
 #define BLOCK_SIZE 256
 #define EPOCHS 1000
-#define LEARNING_RATE 0.001f
+#define LEARNING_RATE 0.0010f
 
 // If this is a CUDA kernel, it MUST have __global__
 __global__ void gradient_descent(
