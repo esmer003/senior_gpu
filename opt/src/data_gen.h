a11cab5049ffab872b7c305416e5606220ae4f91
esmer003 <126800729+esmer003@users.noreply.github.com>
2026-05-01 00:42:43 +0000

    data gen header in src

---
+++ opt/src/data_gen.h
@@ -0,0 +1,11 @@
+#ifndef DATA_GEN_H
+#define DATA_GEN_H
+
+#define TRUE_A 3.0f
+#define TRUE_B -2.0f
+#define TRUE_C 1.5f
+#define TRUE_BIAS 2.0f
+
+void generate_data(float *h_x, float *h_y, int n);
+
+#endif
