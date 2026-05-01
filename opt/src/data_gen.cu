6a4bda65ef56f6d127d1f0e9fa5688444603205a
esmer003 <126800729+esmer003@users.noreply.github.com>
2026-05-01 00:16:11 +0000

    cubic

--- opt/src/data_gen.cu
+++ opt/src/data_gen.cu
@@ -4,12 +4,12 @@
 #define TRUE_BIAS 2.0f
 
 //generate synthetic data for training
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        float x = (float)i/n;
-        h_x[i] = x;
-        h_x2[i] = x * x;
-        h_x3[i] = x * x * x;
-        float noise = (((float)rand()/RAND_MAX - 0.5f) * 0.5f);
-        h_y[i] = TRUE_A * h_x3[i] + TRUE_B * h_x2[i] + TRUE_C * h_x[i] + TRUE_BIAS + noise; //TRUE_W * h_x[i] + TRUE_B;// + (((float)rand()/RAND_MAX - 0.5f) * 0.1f);
-    }
+        h_x[i] = (float)i / n;
+        // Cubic: y = 3x^3 - 2x^2 + 1.5x + 2
+        h_y[i] = 3.0f * h_x[i] * h_x[i] * h_x[i]
+                - 2.0f * h_x[i] * h_x[i]
+                + 1.5f * h_x[i]
+                + 2.0f;
+}
